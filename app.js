898b2f80e9b5c65c7e5c516781de0a5f017f4eef
FallenSG <shivgarg413@gmail.com>
2021-01-29 17:32:53 +0000

    sample

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 var app = require('express')();
+const bodyParser = require('body-parser');
+// var mongoose = require('mongoose');
 
-var index = require('./route/index.js');
-var sign_in = require('./route/sign_in.js');
-var sign_up = require('./route/sign_up.js');
+// mongoose.connect('mongodb://mongodb:27017/sample', { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use('/', index);
-app.use('/sign_in', sign_in);
-app.use('/sign_up', sign_up);
 
-app.listen(3000);
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function () {
+//    // we're connected!
+//    console.log('Connected to mongo db');
+// });
+
+// var personSchema = mongoose.Schema({
+//    name: String,
+//    pass: String
+// });
+// var Person = mongoose.model("Person", personSchema);
+
+var routes = require('./route/index');
+
+app.use('/', routes);
+
+app.listen(3000, function(req, res){
+    console.log('listening...')
+});
