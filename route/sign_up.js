4277afd1acf3f342986a98ae3a3ca9e353482f1c
FallenSG <shivgarg413@gmail.com>
2021-01-29 16:58:13 +0000

    changes

--- route/sign_up.js
+++ route/sign_up.js
@@ -1,7 +1,29 @@
 var router = require('express').Router();
 
 router.get('/', function(req, res){
-    res.send('Got to sign_up');
+    res.render('sign_up');
+});
+
+router.post('/', function(req, res){
+    res.send(req.body);
+    // var personInfo = req.body;
+
+    // if(!personInfo.id || !personInfo.password){
+    //     res.send('error!!!  empty field');
+    // }
+    // else{
+    //     var newPerson = new Person({
+    //         name: personInfo.id,
+    //         pass: personInfo.password
+    //     });
+
+    //     newPerson.save(function(err, Person){
+    //         if(err)  
+    //             res.send('error.. during db');
+    //         else
+    //             res.send('saved');
+    //     });
+    // }
 });
 
 module.exports = router;
